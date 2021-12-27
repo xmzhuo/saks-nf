6d0aa71b44ccb5abd582dfaf5ae13f92526f2227
xmzhuo <xmzhuo@gmail.com>
2021-12-27 12:45:58 -0500

    Add files via upload

--- sak_data/test.sh
+++ sak_data/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
-cat $1 \
-| gatk BedToIntervalList -I /dev/stdin -O $1.interval_list -SD $2 
-
+#cat $1 \
+#| gatk BedToIntervalList -I /dev/stdin -O $1.interval_list -SD $2 
+gatk BedToIntervalList -I $1 -O $1.interval_list -SD $2 
